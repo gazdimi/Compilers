759a6eff17ed83f3b28f38f9706130d7c05f7264
dimitra <dgazepidi@gmail.com>
2021-02-13 18:34:53 +0200

    Apply rule with single part
    construct generated output  by applying one rule at a time

--- Subject2/Automaton.h
+++ Subject2/Automaton.h
@@ -3,13 +3,18 @@
 #include <string>
 #include <iostream>
 #include <random>
+#include <algorithm>
+#include <list>
 
 class Automaton
 {
 private:
 	std::map< std::string, std::map<int, std::vector<std::string> > > rules;
-	std::vector<std::string> generated_output;
+	std::list<std::string> generated_output;
+	std::vector<std::string> terminals;
+	std::vector<std::string> non_terminals;
 	int randomChoice(const int begin, const int end);
+	void Start();
 public:
 	Automaton();
 	~Automaton();
@@ -17,9 +22,10 @@ public:
 
 Automaton::Automaton()
 {
-	std::vector<std::string> labels = {"<E>", "<Y>", "<A>", "<B>"};	//non-terminals, left-side of rules
+	terminals = {"(", ")", "v", "-", "+"};
+	non_terminals = {"<E>", "<Y>", "<A>", "<B>"};			//non-terminals, left-side of rules
 	std::map<int, std::vector<std::string> > production;		//right-side of rules
-	for(int i=0; i<labels.size(); ++i)
+	for(int i=0; i<non_terminals.size(); ++i)
 	{
 		if(i==0){						//E
 			production[0] = {"(","<Y>",")"};
@@ -33,13 +39,13 @@ Automaton::Automaton()
 			production[1] = {"+","<Y>"};
 			production[2] = {"e"};		
 		}
-		rules[labels[i]] = production;
+		rules[non_terminals[i]] = production;
 	}
 	
 	for(auto i = rules["<E>"][0].begin(); i<rules["<E>"][0].end(); i++){ generated_output.push_back(*i); 	}	//rules["<E>"][0][0]
 	//for(auto i = generated_output.begin(); i != generated_output.end(); ++i){ std::cout << *i << std::endl;}
 	
-	std::cout << randomChoice(1, 5) << std::endl;	
+	Start();
 
 	/*for (auto rule : rules)
 	{
@@ -60,15 +66,44 @@ Automaton::Automaton()
 
 Automaton::~Automaton(){ }
 
+void Automaton::Start()												//start generating
+{
+	std::vector<std::string> temp_non_terminals;
+	for(auto i = generated_output.begin(); i != generated_output.end(); ++i)
+	{
+		if(std::find(non_terminals.begin(), non_terminals.end(), *i) != non_terminals.end()) {
+			temp_non_terminals.push_back(*i);							/* generated output contains a non terminal symbol */
+		}
+	}
+
+	if(temp_non_terminals.size() ==  0)									//no more rules can be applied, only terminals included in generated output 
+	{
+		std::cout << "My job here is done!" << std::endl;	
+	}else if(temp_non_terminals.size() ==  1){								//only one rule can be applied
+		
+		if(temp_non_terminals[0]=="<E>" | temp_non_terminals[0]=="<Y>"){				//rule has only one production part
+			auto it = std::find(generated_output.begin(), generated_output.end(), temp_non_terminals[0]);
+		    	if (it != generated_output.end()) {
+				it = generated_output.erase(it);
+				for (int j=0; j<rules[temp_non_terminals[0]][0].size(); ++j)
+				{
+					it = generated_output.insert(it, rules[temp_non_terminals[0]][0][j]);
+					++it;
+				}
+				for (auto it2=generated_output.begin(); it2!=generated_output.end(); ++it2)
+					std::cout << ' ' << *it2;
+					
+				std::cout << std::endl;
+			}
+		}else{												//rule has 2 or more production parts, we need to choose a random part among them
+		}
+	}else{}
+}
+
 int Automaton::randomChoice(const int begin, const int end)
 {
 	std::random_device random_dev;
 	std::mt19937 generator(random_dev());
 	std::uniform_int_distribution<int>  distr(begin, end);
 	return distr(generator);
-	
-	//std::srand(time(0));                                             //pseudo-random number generator using time as an unpredictable seed
-	//return rand();
-
-
 }
