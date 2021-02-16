f82d96c6f09c90cc0afc8518a8867e8e58d4dc0d
stefanos007 <32597164+stefanos007@users.noreply.github.com>
2021-02-16 21:12:14 +0200

    Add files via upload

---
+++ Sub3/Subject1.cpp
@@ -0,0 +1,18 @@
+#include <queue>
+#include <string>
+#include "Automaton.h"
+
+int main(int argc,char *argv[])
+{
+    std::queue <std::string> inputQueue;
+    //Parse User Input
+    for (int i = 1; i < argc; ++i) {
+        for (int j = 0; argv[i][j] != '\0'; ++j) {
+        inputQueue.push(std::string(1,argv[i][j]));
+        }
+    }
+    int spaces=20;
+    std::cout << "Stack" << std::string(spaces,' ') << "State" << std::string(spaces,' ') << "Input" << std::string(spaces,' ') << "Rule" << std::endl;
+    Automaton automaton = Automaton();			//initialization of pushdown automaton
+    automaton.Start(inputQueue);			//pass user input to check whether it can be identified
+}
