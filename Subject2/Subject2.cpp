25600528ab3ef7598f218696ae7392dda2c0dae5
dimitra <dgazepidi@gmail.com>
2021-03-02 18:01:18 +0200

    Subject2 done

--- Subject2/Subject2.cpp
+++ Subject2/Subject2.cpp
@@ -5,5 +5,3 @@ int main()
 	
 	Automaton automaton = Automaton();
 }
-//g++ -std=c++11  Subject2.cpp -o Subject2.out		needs c++ version 11 or later in order to be executed from g++ compiler (because of vector's initialization as list)
-//g++ -std=c++14  Subject2.cpp -o Subject2.out
